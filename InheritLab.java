47d4ae66888615ec0d83ab7a5d3298dc7d1a5211
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-05 20:45:39 +0530

    Add files via upload

---
+++ InheritLab.java
@@ -0,0 +1,52 @@
+class person{
+    int id;
+    String name, course, city;
+
+    void setData(int id, String name, String course, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.course = course;
+    }
+
+}
+
+class A extends person{
+    
+  void getData(){
+    System.out.println("Roll Number: "+id+" Name: "+name+" Course: "+course+" City: "+city);
+  }
+}
+
+class B extends person{
+    void getData() {
+      System.out.println("Emp ID: "+id+" Emp Name: "+name+" Emp Course: "+course+" Emp City: "+city);
+    }
+}
+
+class InheritLab {
+    public static void main(String[] args) {
+        A ob = new A();
+        ob.setData(101, "Saurabh", "BCA", "Allahabad");
+        ob.getData();
+        B obj = new B();
+        obj.setData(101, "Anupam Sir", "English", "pune");
+        obj.getData();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
