37d458c6ac2c1c28afbf308cd5f8a17dd0a743d1
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-13 21:35:30 +0530

    Update Alphabet F.java

--- Print Alphabet/Alphabet F.java
+++ Print Alphabet/Alphabet F.java
@@ -1,6 +1,24 @@
 class F {
   public static void main(String[] args) {
     int i,j,k;
-   
+    for(i=0;i<=11;i++)
+      {
+        for(j=0;j<2;j++)
+          {
+            System.out.print("*");
+          }
+        for(k=1;k<=5;k++)
+          {
+            if(i>=0 && i<2 || i>4 && i<7)
+            {
+              printf("**");
+            }
+            else
+            {
+              System.out.printf("  ");//Twp Spaces
+            }
+          }
+        System.out.println("");
+      }
   }
 }
