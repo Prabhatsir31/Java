029a04b16a89eb33658ec79efb62eba9120a25fb
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-13 20:30:56 +0530

    Update Alphabet C.java

--- Print Alphabet/Alphabet C.java
+++ Print Alphabet/Alphabet C.java
@@ -1,6 +1,25 @@
 class C {
   public static void main(String[] args) {
     int i,j,k;   
+    for(i=0;i<=11;i++)
+      {
+        for(j=0;j<=2;j++)
+          {
+            System.out.printf("*");
+          }
+        for(k=0;k<=5;k++)
+          {
+            if(i>=0 && i<2 || i>9 && i<=11)
+            {
+              System.out.printf("**");
+            }
+            else
+            {
+              System.out.printf("  "); //Two Space
+            }
+          }
+        System.out.printf("\n");
+      }
   }
 }
 
