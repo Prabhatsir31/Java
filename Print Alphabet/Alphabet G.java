950e7548cda4607bdc09769bc470dc10b8a5c3a4
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-13 21:57:41 +0530

    Update Alphabet G.java

--- Print Alphabet/Alphabet G.java
+++ Print Alphabet/Alphabet G.java
@@ -1,7 +1,25 @@
 class F {
   public static void main(String[] args) {
     int i,j,k;
-   
+    for(i=0;i<=11;i++)
+      {
+        for(j=0;j<2;j++)
+          {
+            System.out.print("*");
+          }
+        for(k=0;k<=5;k++)
+          {
+            if(i>0 && i<2 || i>4 && i<7 && k>=3 && k<=5 || i>=6 && i<=9 && k==5)
+            {
+              System.out.print("**");
+            }
+            else
+            {
+              System.out.print("  "); //Two Spaces
+            }
+          }
+      }
+    System.out.println();
   }
 }
 
