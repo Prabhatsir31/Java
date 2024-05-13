cf8b7d2d54df26199eccd0c06230d996af4821b1
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-13 21:24:48 +0530

    Update Alphabet E.java

--- Print Alphabet/Alphabet E.java
+++ Print Alphabet/Alphabet E.java
@@ -1,6 +1,25 @@
 class E {
   public static void main(String[] args) {
-   
+    int i,j,k;
+    for(i=0;i<=11;i++)
+      {
+        for(j=0;j<2;j++)
+          {
+            System.out.print("*");
+          }
+        for(k=1;k<=5;k++)
+          {
+            if(i>=0 && i<2 || i>4 && i<7 || i>9 && i<=11)
+            {
+              System.out.print("**");
+            }
+            else
+            {
+              System.out.printf("  ");//Two Space
+            }
+          }
+        System.out.println();
+      }
   }
 }
  
