a278ccc7a1603e3c7f8cca279e5ef89394fbbbd4
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-14 10:17:46 +0530

    Update Alphabet H.java

--- Print Alphabet/Alphabet H.java
+++ Print Alphabet/Alphabet H.java
@@ -1,1 +1,29 @@
+class H {
+  public static void main(String[] args) {
+    int i,j,k;
+    for(i=0;i<=11;i++)
+      {
+        for(j=0;j<2;j++)
+          {
+            System.out.print("*");
+          }
+        for(k=1;k<=5;k++)
+          {
+            if(i>4 && i<7)
+            {
+              System.out.print("**");
+            }
+            else
+            {
+              System.out.print("  "); //Two Spaces
+            }
+          }
+        for(j=0;j<2;j++)
+          {
+            System.out.print("*");
+          }
+        System.out.print("\n");
+      }
+  }
+}
 
