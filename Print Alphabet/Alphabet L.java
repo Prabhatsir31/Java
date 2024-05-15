9deca2d3791e7c6dfd5dd8ea584188131f5d3c27
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-15 16:15:27 +0530

    Update Alphabet L.java

--- Print Alphabet/Alphabet L.java
+++ Print Alphabet/Alphabet L.java
@@ -1,7 +1,25 @@
 class K {
   public static void main(String[] args) {
-    int i,j,k,count=6;
-       
+    int i,j,k;
+    for(i=0;i<=11;i++)       
+      {
+        for(j=0;j<=2;j++)
+          {
+            System.out.printf("*");    
+          }
+        for(k=1;k<=5;k++)
+          {
+            if(i>9 && i<=11)
+            {
+              System.out.printf("**");
+            }
+            else
+            {
+              System.out.printf("  ");//Two Spaces
+            }
+          }
+        System.out.printf("\n");
+      }
   }
 } 
 
