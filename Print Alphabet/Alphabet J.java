a107cd9912eb870ec472b992d0027460877d7137
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-15 11:57:54 +0530

    Update Alphabet J.java

--- Print Alphabet/Alphabet J.java
+++ Print Alphabet/Alphabet J.java
@@ -5,8 +5,16 @@ class I {
       {
         for(j=0;j<=6;j++)
           {
-            if(i>=0 && i<7 || i>=7 && i<=9 && )
+            if(i>=0 && i<2 || i>=2 && i<=9 && j==3 || i>=7 && i<=9 && j==0 || i>9 && i<=11 && j<=3)
+            {
+              System.out.printf("***");
+            }
+            else
+            {
+              System.out.printf("   ");//Three spaces
+            }
           }
+        System.out.printf("\n");
       }
   }
 } 
