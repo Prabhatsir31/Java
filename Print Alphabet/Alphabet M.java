d6f8e16feea5082305d4b0087cf5de54a24397b6
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-16 17:39:44 +0530

    Update Alphabet M.java

--- Print Alphabet/Alphabet M.java
+++ Print Alphabet/Alphabet M.java
@@ -1,1 +1,38 @@
+public class Main {
+  public static void main(String[] args) {
+    int i,j,k,l,m;
+    for(i=0;i<=11;i++)
+      {
+        for(j=0;j<=2;j++)
+          {
+            System.out.printf("*"); //line
+          }
+          for(k=1;k<=i;k++)
+            {
+              System.out.printf(" "); //bottom right
+            }
+          for(j=0;j<=2;j++)
+            {
+              System.out.printf("*"); //line
+            }
+            for(m=10;m>=i;m--)//middle portion
+              {
+                System.out.printf("  "); //two spaces
+              }
+            for(j=0;j<=2;j++)
+              {
+                System.out.printf("*"); //line
+              }
+            for(k=1;k<=i;k++)
+              {
+                System.out.printf("*"); //line
+              }
+            for(j=0;j<=2;j++)
+              {
+                System.out.printf("*"); //line
+              }
+        System.out.printf("\n");
+      }
+  }
+}
 
