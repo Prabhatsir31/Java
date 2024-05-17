31db6f7113d36c73afb24c41438a505a46293e28
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-17 11:45:04 +0530

    Update Alphabet N.java

--- Print Alphabet/Alphabet N.java
+++ Print Alphabet/Alphabet N.java
@@ -1,1 +1,31 @@
+class N {
+  public static void main(String[] args) {
+    int i,j,k,l;
+    for(i=0;i<=11;i++)
+      {
+        for(j=0;j<=2;j++)
+          {
+            System.out.printf("*");
+          }
+         for(k=1;k<=i;k++)
+           {
+             System.out.printf(" ");
+           }
+          for(j=0;j<=2;j++)
+            {
+              System.out.printf("*");
+            }
+          for(l=10;l>=i;l--)
+            {
+              System.out.printf(" ");
+            }
+          for(j=0;j<=2;j++)
+            {
+              System.out.printf("*");
+            }
+        System.out.printf("\n");
+      }
+  }
+}
+
 
