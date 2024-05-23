f4a4676288c5b299ec205c23ba33bfabf9657b44
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-05-23 23:14:48 +0530

    Update Alphabet S.java

--- Print Alphabet/Alphabet S.java
+++ Print Alphabet/Alphabet S.java
@@ -1,1 +1,17 @@
+class S {
+  public static void main(String[] args) {
+    int i,j,k,count=11;
+    for(i=0;i<=11;i++)   
+      {
+                 
+        System.out.printf("\n");
+      }
+  }
+}
+
+
+
+
+
+
 
