d8bef27e6f43a091d501e7b8dd3c83a08f46a5aa
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-06-14 00:25:35 +0530

    Create Alphabet Y.java

---
+++ Print Alphabet/Alphabet Y.java
@@ -0,0 +1,12 @@
+class Y
+  {
+    public static void main(String[] args)
+    {
+      int i,j;
+      for(i=0;i<=11;i++)
+        {
+          
+          System.out.printf("\n");
+        }
+    }
+  }
