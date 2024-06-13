be3a0bda49c7a771cbb8fd0b331f048859288c67
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-06-14 00:22:09 +0530

    Update Alphabet X.java

--- Print Alphabet/Alphabet X.java
+++ Print Alphabet/Alphabet X.java
@@ -1,10 +1,31 @@
-class W
+class X
   {
     public static void main(String[] args)
     {
-      int i,j,k,l;
+      int i,j,count1=1,count2=10;
       for(i=0;i<=11;i++)
         {
+          for(j=0;j<=11;j++)
+            {
+              if(j==count1 || j==count2)
+              {
+                System.out.printf("***");
+              }
+              else
+              {
+                System.out.printf("  "); //two space
+              }
+            }
+          if(i<=4)
+          {
+            count1++;
+            count2--;
+          }
+          else if(i<=11)
+          {
+            count1--;
+            count2++;
+          }
           System.out.printf("\n");
         }
     }
