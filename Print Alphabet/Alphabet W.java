c92d73f1fc44d8415012cfb5895f590abdf8c71d
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-06-14 00:07:05 +0530

    Update Alphabet W.java

--- Print Alphabet/Alphabet W.java
+++ Print Alphabet/Alphabet W.java
@@ -1,1 +1,39 @@
-
+class W
+  {
+    public static void main(String[] args)
+    {
+      int i,j,k,l,m;
+      for(i=0;i<=11;i++)
+        {
+          for(j=0;j<=2;j++)
+            {
+              System.out.printf("*");
+            }
+          for(k=10;k>=i;k--)
+            {
+              System.out.printf(" ");//single space
+            }
+          for(j=0;j<=2;j++)
+            {
+              System.out.printf("*");
+            }
+          for(l=1;l<=i;l++)
+            {
+              System.out.printf("  ");//two space
+            }
+           for(j=0;j<=2;j++)
+            {
+              System.out.printf("*");
+            }
+          for(k=10;k>=i;k--)
+            {
+              System.out.printf(" ");//single space
+            }
+          for(j=0;j<=2;j++)
+            {
+              System.out.printf("*");
+            }
+          System.out.printf("\n");
+        }
+    }
+  }
