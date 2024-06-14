388455c4f4eb0662b7bba7eecb0aa3aa37af54a6
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-06-14 16:38:17 +0530

    Update Alphabet Z.java

--- Print Alphabet/Alphabet Z.java
+++ Print Alphabet/Alphabet Z.java
@@ -2,10 +2,21 @@ class Z
   {
     public static void main(String[] args)
     {
-      int i,j,c1=1,c2=10;
+      int i,j,c=11;
       for(i=0;i<=11;i++)
         {
-          
+          for(j=0;j<=11;j++)
+            {
+              if(i>=0 && i<2 || i>9 && i<=11 || i>=2 && i<=9 && j==c)
+              {
+                System.out.printf("***");      
+              }
+              else
+              {
+                System.out.printf("  ");
+              }
+            }
+          c=c-1;
           System.out.printf("\n");
         }
     }
