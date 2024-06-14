d36463166d3c8131c0a754aa9647bee811f789ce
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-06-14 16:33:58 +0530

    Create Alphabet Z.java

---
+++ Print Alphabet/Alphabet Z.java
@@ -0,0 +1,12 @@
+class Z
+  {
+    public static void main(String[] args)
+    {
+      int i,j,c1=1,c2=10;
+      for(i=0;i<=11;i++)
+        {
+          
+          System.out.printf("\n");
+        }
+    }
+  }
