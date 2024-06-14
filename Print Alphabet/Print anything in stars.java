135d5fbc823dbddc5700acda186524db0158b1db
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-06-14 17:08:21 +0530

    Update Print anything in stars.java

--- Print Alphabet/Print anything in stars.java
+++ Print Alphabet/Print anything in stars.java
@@ -1,1 +1,78 @@
+import java.util.*;
+import java.lang.*;
+class anyname
+  {
+    static int i,j,k,l,count=6,c=1,c1=10,c2=1,c3=10,c4=11,c5=0,c6=8,a=0;
 
+    static void line() //create a line
+    {
+      for(k=0;k<=2;k++)
+        {
+          System.out.printf("*");
+        }
+    }
+
+
+    static coid gap() //create space
+    {
+      for(k=0;k<=2;k++)
+        {
+          System.out.printf(" ");
+        }
+    }
+
+
+    static void crosstr() //cross triangle top (right)
+    {
+      for(j=10;j>=i;j--)
+        {
+          System.out.printf(" ");
+        }
+    }
+
+
+    static void crosstl() //cross triangle (left)
+    {
+      for(j=10;j>=i;j--)
+        {
+          System.out.printf(" ");
+        }
+    }
+
+
+    static void crossbr() //cross triangle bottom (right)
+    {
+      for(l=1;l<=i;l++)
+        {
+          System.out.printf(" ");
+        }
+    }
+
+
+    static void crossbl() //cross triangle bottom (left)
+    {
+      for(j=1;j<=i;j++)
+        {
+          System.out.printf(" ");
+        }
+    }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  }
