e624facf6734f3f6d17a5698fb9729861d0db61b
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2024-07-09 00:05:29 +0530

    Update Print anything in stars.java

--- Print Alphabet/Print anything in stars.java
+++ Print Alphabet/Print anything in stars.java
@@ -338,14 +338,330 @@ class anyname
     
     
     
+    static void midp() //middle part of P
+    {
+    	for(l=1;l<=5;l++)
+        {
+        	if(i>=0 && i<2 || i>4 && i<7 || i>=2 && i<=4 && l==5)
+            {
+            	System.out.printf("**");
+            }
+            else
+            {
+            	System.out.printf("  ");
+            }
+        }
+    }
     
     
     
+    static void spq() //creates Q
+    {
+    	for(l=0;l<=11;l++)
+        {
+        	if(i>=0 && i<2 && l<9 || i>=2 && i<9 && l==0 || l==8 && i<10 || i>7 && i<=9 && l<9 || i>=9 && i<=11 && l==i)
+            {
+            	System.out.printf("**");
+            }
+            else 
+            {
+            	System.out.printf("  ");//two spaces
+            }
+        }
+    }
+    
+    
+  
+    static void midr() //middle part of R
+    {
+    	for(l=1;l<=5;l++)
+        {
+        	if(i>=0 && i<2 || i>4 && i<7 || i>=2 && i<=4 && l==5 || i>=7 && i<=11 && l==c4)
+            {
+            	System.out.printf("**");
+            }
+            else
+            {
+            	System.out.printf("  ");
+            }
+        }
+        
+        if(i>6)
+        {
+        	c4++;
+        }
+        else
+        {
+        	c4=1;
+        }
+    }
     
     
     
-}        
+    static void sps() //creates S
+    {
+    	for(l=1;l<=8;l++)
+        {
+        	if(i>=0 && i<2 || i>4 && i<7 || i>=2 && i<=4 && l==1 || i>6 && i<=9 && l==8 || i>9 && i<=11)
+            {
+            	System.out.printf("**");
+            }
+            else
+            {
+            	System.out.printf("  ");
+            }
+        }
+    }
     
     
+    static void spt() // creates T
+    {
+    	for(l=0;l<=6;l++)
+        {
+        	if(i>=0 && i<2 || i>=2 && i<=11 && l==3)
+            {
+            	System.out.printf("***");
+            }
+            else
+            {
+            	System.out.printf("   ");
+            }
+        }
+    }
     
-   
+    static void midu() // middle part of U
+    {
+    	for(l=1;l<=6;l++)
+        {
+        	if(i>9 && i<=11)
+            {
+            	System.out.printf("**");
+            }
+            else
+            {
+            	System.out.printf("  ");
+            }
+        }
+    }
+    
+    
+    static void midw() //middle part of W
+    {
+    	for(j=1;j<=i;j++)
+        {
+        	System.out.printf("  ");
+        }
+    }
+    
+    static void spx() // creates X
+    {
+    	for(l=0;l<=11;l++)
+        {
+        	if(l==c || l==c1)
+            {
+            	System.out.printf("***");
+            }
+            else
+            {
+            	System.out.printf("  ");
+            }
+        }
+        
+        if(i<=4)
+        {
+        	c++;
+            c1--;
+        }
+        else if(i<=11)
+        {
+        	c1++;
+            c--;
+        }
+    }
+    
+    
+    static void spy() // creates Y
+    {
+    	for(l=0;l<=11;l++)
+        {
+        	if(l==c2 || l==c3 && i<5)
+            {
+            	System.out.printf("***");
+            }
+            else
+            {
+            	System.out.printf("  ");
+            }
+        }
+        
+        if(i<=4)
+        {
+        	c2++;
+            c3--;
+        }
+        else if(i<=11)
+        {
+        	c3++;
+        }
+        if(i>=5)
+        {
+        	System.out.printf(" ");
+        }
+    }
+    
+    
+    static void spz() // creates Z
+    {
+    	for(l=0;l<=11;l++)
+        {
+        	if(i>=0 && i<2 || i>9 && i<=11 || i>=2 && i<=9 && l==c4)
+            {
+            	System.out.printf("**");
+            }
+            else
+            {
+            	System.out.printf("  ");
+            }
+        }
+        c4=c4-1;
+    }
+    
+    
+    public static void main(String[] args)
+    {
+    	Scanner sc = new Scanner(System.in);
+        c=1;c1=10;c2=1;c3=10;c4=11;
+        String aa;
+        System.out.print("----------------\n(-: By SMB Corporation  :-)\n------------------\n");
+        System.out.print("Enter Name [In Lower Case]: ");
+        aa=sc.nextLine();
+        char[] cc=aa.toCharArray();
+        System.out.print("\n");
+        for(i=0;i<=11;i++)
+        {
+        	for(a=0;a<cc.length;a++)
+            {
+            	if(cc[a]=='a')
+                {
+    				crosstr();line();mida();line();crosstl();gap();            	
+                }
+                else if(cc[a]=='b')
+                {
+                	line();midb();gap();
+                }
+                else if(cc[a]=='c')
+                {
+                	line();midc();gap();
+                }
+                else if(cc[a]=='d')
+                {
+                	line();midd();gap();
+                }
+                else if(cc[a]=='e')
+                {
+                	line();mide();gap();
+                }
+				else if(cc[a]=='f')
+                {
+                	line();midf();gap();
+                }
+                else if(cc[a]=='g')
+                {
+                	line();midg();gap();
+                }
+                else if(cc[a]=='h')
+                {
+                	line();midh();line();gap();
+                }
+                else if(cc[a]=='i')
+                {
+                	spi();gap();
+                }
+                else if(cc[a]=='j')
+                {
+                	spj();gap();
+                }
+                else if(cc[a]=='k')
+                {
+                	line();midk();gap();
+                }
+                else if(cc[a]=='l')
+                {
+                	line();midl();gap();
+                }
+                else if(cc[a]=='m')
+                {
+                	line();crossbr();line();midv();line();crossbl();line();gap();
+                }
+                else if(cc[a]=='n')
+                {
+                	line();crossbr();line();crosstl();line();gap();
+                }
+                else if(cc[a]=='o')
+                {
+                	mido();gap();
+                }
+                else if(cc[a]=='p')
+                {
+                	line();midp();gap();
+                }
+                else if(cc[a]=='q')
+                {
+                	spq();gap();
+                }
+                else if(cc[a]=='r')
+                {
+                	line();midr();gap();
+                }
+                else if(cc[a]=='s')
+                {
+                	sps();gap();
+                }
+                else if(cc[a]=='t')
+                {
+                	spt();gap();
+                }
+                else if(cc[a]=='u')
+                {
+                	line();midu();line();gap();
+                }
+                else if(cc[a]=='v')
+                {
+                	crossbr();line();midv();line();crossbl();gap();
+                }
+                else if(cc[a]=='w')
+                {
+                	line();crosstr();line();midw();line();crosstl();line();gap();
+                }
+                else if(cc[a]=='x')
+                {
+                	spx();gap();
+                }
+                else if(cc[a]=='y')
+                {
+                	spy();gap();
+                }
+                else if(cc[a]=='z')
+                {
+                	spz();gap();
+                }
+                else if(cc[a]==' ')
+                {
+                	gap();gap();
+                }
+            }
+            System.out.print("\n");
+        }
+        
+        System.out.println("\n\n-----------------------------------------------------\nLooping Sucessfull \n");
+        java1.main(args);// calling main function
+    }    
+    
+}    
+
+
+
+
+
+
+    
